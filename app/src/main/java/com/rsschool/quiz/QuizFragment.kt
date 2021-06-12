45e36c1d5b5007388a7f2930a809925df4d5dcf4
jerubrin <jerubbaal@mail.ru>
2021-06-12 21:10:54 +0600

    feat: add navigation forward and back with IntArray in Args

--- app/src/main/java/com/rsschool/quiz/QuizFragment.kt
+++ app/src/main/java/com/rsschool/quiz/QuizFragment.kt
@@ -4,12 +4,20 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RadioButton
+import androidx.core.view.forEach
+import androidx.core.view.forEachIndexed
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import com.rsschool.quiz.databinding.FragmentQuizBinding
 
 class QuizFragment : Fragment() {
     private var _binding: FragmentQuizBinding? = null
     private val binding get() = _binding!!
+
+    var keysArray :IntArray? = null
+    private var selectedAns = -1
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -27,5 +35,36 @@ class QuizFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val fragmentArgs : QuizFragmentArgs by navArgs()
+        keysArray = fragmentArgs.keys
+        if (keysArray != null) binding.previousButton.isEnabled = true
+
+        //--------------------------
+        binding.question.text = "["
+        keysArray?.forEach {
+            binding.question.text = "${binding.question.text} $it"
+        }
+        binding.question.text = "${binding.question.text}]"
+        //--------------------------
+
+        binding.radioGroup.setOnCheckedChangeListener { group, _ ->
+            group.forEachIndexed { index, viewRB ->
+                if ((viewRB as RadioButton).isChecked) {
+                    binding.nextButton.isEnabled = true
+                    selectedAns = index
+                }
+            }
+        }
+
+        binding.nextButton.setOnClickListener {
+            keysArray = if (keysArray == null) intArrayOf(selectedAns) else keysArray?.plus(intArrayOf(selectedAns))
+            val actionForward = QuizFragmentDirections.actionQuizFragmentSelf(keysArray)
+            findNavController().navigate(actionForward)
+        }
+
+        binding.previousButton.setOnClickListener {
+            findNavController().popBackStack()
+        }
     }
 }

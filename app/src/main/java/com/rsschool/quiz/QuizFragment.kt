64c9561724d161e7205c0ca2ebe4582145e55296
Jerubrin <jerubbaal@mail.ru>
2021-06-13 18:09:09 +0600

    fix: working forward and back, with saving data

--- app/src/main/java/com/rsschool/quiz/QuizFragment.kt
+++ app/src/main/java/com/rsschool/quiz/QuizFragment.kt
@@ -5,12 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.RadioButton
+import androidx.core.os.bundleOf
 import androidx.core.view.forEach
 import androidx.core.view.forEachIndexed
+import androidx.core.view.get
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.rsschool.quiz.databinding.FragmentQuizBinding
+import kotlinx.coroutines.handleCoroutineException
 
 class QuizFragment : Fragment() {
     private var _binding: FragmentQuizBinding? = null
@@ -18,6 +21,7 @@ class QuizFragment : Fragment() {
 
     private var selectedAns = -1
     private var currentQuestion = 0
+    private var keysArray :IntArray? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -42,17 +46,13 @@ class QuizFragment : Fragment() {
 
         if (currentQuestion != 0) binding.previousButton.isEnabled = true
 
-        var keysArrayBack = findNavController().currentBackStackEntry?.savedStateHandle?.getLiveData<IntArray>("answers")?.value
-        if (keysArrayBack != null)
-            keysArray = keysArrayBack
-
-        //--------------------------
-        binding.question.text = "["
-        keysArray?.forEach {
-            binding.question.text = "${binding.question.text} $it"
+        val wasSelecting = getSelectedFromKeysArray(keysArray, currentQuestion)
+        if (wasSelecting >= 0) {
+            var rb: RadioButton = binding.radioGroup[wasSelecting] as RadioButton
+            rb.setChecked(true)
+            binding.nextButton.isEnabled = true
+            selectedAns = wasSelecting
         }
-        binding.question.text = "${binding.question.text}]"
-        //--------------------------
 
         binding.radioGroup.setOnCheckedChangeListener { group, _ ->
             group.forEachIndexed { index, viewRB ->
@@ -61,15 +61,11 @@ class QuizFragment : Fragment() {
                     selectedAns = index
                 }
             }
-            if (keysArray != null) {
-                val keysArrayToBack = keysArray?.plus(intArrayOf(selectedAns))
-                findNavController().previousBackStackEntry?.savedStateHandle?.set("answers", keysArrayToBack)
-            }
+            keysArray = setKeyToKeyArray(keysArray, selectedAns, currentQuestion)
         }
 
         binding.nextButton.setOnClickListener {
-            keysArray = if (keysArray == null) intArrayOf(selectedAns) else keysArray?.plus(intArrayOf(selectedAns))
-            val actionForward = QuizFragmentDirections.actionQuizFragmentSelf(keysArray)
+            val actionForward = QuizFragmentDirections.actionQuizFragmentSelf(keysArray, currentQuestion + 1)
             findNavController().navigate(actionForward)
         }
 
@@ -78,7 +74,44 @@ class QuizFragment : Fragment() {
         }
     }
 
-    companion object {
-        var keysArray :IntArray? = null
+    override fun onResume() {
+        super.onResume()
+
+        var keysArrayBack =
+            findNavController().currentBackStackEntry?.savedStateHandle?.getLiveData<IntArray>("answersBack")?.value
+        if (keysArrayBack != null) {
+            keysArray = keysArrayBack
+        }
+        //--------------------------
+        binding.question.text = "["
+        keysArray?.forEach {
+            binding.question.text = "${binding.question.text} $it"
+        }
+        binding.question.text = "${binding.question.text}] currentQ=$currentQuestion"
+        //--------------------------
+        keysArray = setKeyToKeyArray(keysArray, selectedAns, currentQuestion)
+    }
+
+    private fun setKeyToKeyArray(intArray: IntArray?, answer: Int, currentQ: Int): IntArray? {
+        var newArray = intArray
+        if (newArray == null) {
+            newArray = intArrayOf(answer)
+        } else {
+            if (newArray?.lastIndex!! < currentQ) {
+                newArray = newArray?.plus(intArrayOf(answer))
+            } else {
+                newArray!![currentQ] = answer
+            }
+        }
+        if (newArray != null) {
+            findNavController().previousBackStackEntry?.savedStateHandle?.set("answersBack", newArray)
+        }
+        return newArray
+    }
+
+    private fun getSelectedFromKeysArray(intArray: IntArray?, currentQ: Int): Int {
+        var newArray: IntArray? = intArray ?: return -1
+        if (newArray?.lastIndex!! < currentQ) return -1
+        return newArray[currentQ]
     }
 }
